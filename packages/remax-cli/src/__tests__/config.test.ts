fd295b8fc4d8a912bc62c5b35ee8d26c39fcdec9
Caihuanyu <eterlf41@gmail.com>
2020-02-10 10:43:12 +0800

    feat(cli): 引入 static compiler

--- packages/remax-cli/src/__tests__/config.test.ts
+++ packages/remax-cli/src/__tests__/config.test.ts
@@ -18,7 +18,7 @@ describe('remax config', () => {
     }).toThrowErrorMatchingInlineSnapshot(`
 "Invalid configuration object. remax has been initialized using a configuration object that does not match the API schema.
  - configuration has an unknown property 'xxx'. These properties are valid:
-   object { cssModules?, notify?, pxToRpx?, cwd?, progress?, compressTemplate?, output?, rootDir?, UNSAFE_wechatTemplateDepth?, alias?, postcss?, rollupOptions? }
+   object { compiler?, cssModules?, notify?, pxToRpx?, cwd?, progress?, compressTemplate?, output?, rootDir?, UNSAFE_wechatTemplateDepth?, alias?, postcss?, rollupOptions? }
  - configuration.cssModules should be one of these:
    RegExp | boolean
    Details:
--- packages/remax-cli/src/build/plugins/components.ts
+++ packages/remax-cli/src/build/plugins/components.ts
@@ -2,6 +2,11 @@ import * as t from '@babel/types';
 import { NodePath } from '@babel/traverse';
 import { kebabCase, cloneDeep } from 'lodash';
 import API from '../../API';
+import { RemaxOptions } from '../..';
+import {
+  LEAF_ATTRIBUTE_NAME,
+  ENTRY_ATTRIBUTE_NAME,
+} from './compiler/static/constants';
 
 export interface Component {
   id: string;
@@ -102,6 +107,9 @@ function registerProps(componentName: string, node?: t.JSXElement) {
       usedProps
         // 无需收集 slot 字段
         .filter(p => p !== 'slot')
+        // 静态编译辅助字段
+        .filter(p => p !== LEAF_ATTRIBUTE_NAME)
+        .filter(p => p !== ENTRY_ATTRIBUTE_NAME)
         .filter(Boolean)
         .map(prop => hostComponent?.alias?.[prop] || prop)
     ),
@@ -114,7 +122,9 @@ function registerComponent(
     node,
     importer = '',
     phase,
+    remaxOptions,
   }: {
+    remaxOptions: RemaxOptions;
     componentName: string;
     node?: t.JSXElement;
     importer?: string;
@@ -122,7 +132,14 @@ function registerComponent(
   },
   additional?: boolean
 ) {
-  if (!API.shouldHostComponentRegister(componentName, phase, additional)) {
+  if (
+    !API.shouldHostComponentRegister(
+      remaxOptions,
+      componentName,
+      phase,
+      additional
+    )
+  ) {
     return;
   }
 
@@ -136,12 +153,13 @@ function registerComponent(
     id: componentName,
     props,
     importer,
+    additional,
   };
 
   addToComponentCollection(component, importers);
 }
 
-export default () => {
+export default (options: RemaxOptions) => {
   importers.clear();
 
   return {
@@ -161,6 +179,7 @@ export default () => {
             const componentName = specifier.imported.name;
             const id = kebabCase(componentName);
             registerComponent({
+              remaxOptions: options,
               componentName: id,
               importer: state.file.opts.filename,
               phase: 'import',
@@ -193,6 +212,7 @@ export default () => {
           const id = kebabCase(componentName);
 
           registerComponent({
+            remaxOptions: options,
             componentName: id,
             node,
             importer: state.file.opts.filename,
@@ -204,10 +224,11 @@ export default () => {
   };
 };
 
-export function getComponents() {
+export function getComponents(options: RemaxOptions) {
   API.getHostComponents().forEach((component, componentName) => {
     registerComponent(
       {
+        remaxOptions: options,
         componentName,
         phase: 'extra',
       },
