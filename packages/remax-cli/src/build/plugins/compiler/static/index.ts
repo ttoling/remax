8c54e96b1ae1ec7e616136668a9314d7a8113320
Caihuanyu <eterlf41@gmail.com>
2020-02-24 21:04:02 +0800

    fix error

--- packages/remax-cli/src/build/plugins/compiler/static/index.ts
+++ packages/remax-cli/src/build/plugins/compiler/static/index.ts
@@ -2,4 +2,3 @@ export { default as preprocess } from './preprocess';
 export { default as visit, jsxElementPathSet } from './visit';
 export { default as renderTemplates } from './render/templates';
 export { renderCommon, renderPage } from './render/asset';
-export { default as constants } from './constants';
